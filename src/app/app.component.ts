7c73857a245aac9d0174b71cce76ee8503b6951f
lukzap <lukasz.zaparucha@gmail.com>
2020-01-17 21:50:12 +0100

    expandable menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shortly';
+  @ViewChild('menu', {static: false}) menu: ElementRef;
+
+  showMenu() {
+    this.menu.nativeElement.classList.toggle('menu--closed');
+  }
 }
