f85f4d219f9344babf9839ee3c15d6c321704185
lukzap <lukasz.zaparucha@gmail.com>
2020-01-18 02:38:32 +0100

    top-menu responsive stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({opacity: 0}),
+          animate('100ms', style({ opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({opacity: 1}),
+          animate('100ms', style({ opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
 export class AppComponent {
+  showMenu = false;
   title = 'shortly';
-  @ViewChild('menu', {static: false}) menu: ElementRef;
-
-  showMenu() {
-    this.menu.nativeElement.classList.toggle('menu--closed');
-  }
 }
